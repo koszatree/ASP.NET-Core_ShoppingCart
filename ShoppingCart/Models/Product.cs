e76bc262ec9c1e39d4244ef4fdfd4bb102b2cc5c
koszatree <koszatree@gmail.com>
2023-11-05 14:55:02 +0100

    FileExtensionAttribute

--- ShoppingCart/Models/Product.cs
+++ ShoppingCart/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ShoppingCart.Infrastructure.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShoppingCart.Models
@@ -23,5 +24,9 @@ namespace ShoppingCart.Models
         public long CategoryId { get; set; }
         public Category Category { get; set; }
         public string Image { get; set; }
+
+        [NotMapped]
+        [FileExtension]
+        public IFormFile ImageUpload { get; set; }
     }
 }
