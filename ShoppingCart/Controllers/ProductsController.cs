e9f8562c2928bbe4625dfdc1d9b5dc34808b9573
koszatree <koszatree@gmail.com>
2023-11-06 16:18:36 +0100

    Create Product GET ** To fix: Areas/../Index.cshtml - Category Name Areas/../Create.cshtml | wwwroot/js/site.js - Image Preview

--- ShoppingCart/Controllers/ProductsController.cs
+++ ShoppingCart/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ShoppingCart.Infrastructure;
 using ShoppingCart.Models;
@@ -15,28 +16,18 @@ namespace ShoppingCart.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index(string categorySlug = "", int p = 1)
+        public async Task<IActionResult> Index(int p = 1)
         {
             int pageSize = 3;
             ViewBag.PageNumber = p;
             ViewBag.PageRange = pageSize;
-            ViewBag.CategorySlug = categorySlug;
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.Products.Count() / pageSize);
 
-            if(categorySlug == "")
-            {
-                ViewBag.TotalPages = (int)Math.Ceiling((decimal)_context.Products.Count()/pageSize);
-                return View(await _context.Products.OrderByDescending(p => p.Id).Skip((p-1)*pageSize).Take(pageSize).ToListAsync());
-            }
-
-            Category category = await _context.Categories.Where(c => c.Slug == categorySlug).FirstOrDefaultAsync();
-            if (category == null) return RedirectToAction("Index");
-
-            var productsByCategory = _context.Products.Where(p => p.CategoryId == category.Id);
-            ViewBag.TotalPages = (int)Math.Ceiling((decimal)productsByCategory.Count() / pageSize);
-
-            return View(await productsByCategory.OrderByDescending(p => p.Id).Skip((p - 1) * pageSize).Take(pageSize).ToListAsync());
+            return View(await _context.Products.OrderByDescending(p => p.Id)
+                .Include(p => p.Category)
+                .Skip((p - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync());
         }
-
-
     }
 }
